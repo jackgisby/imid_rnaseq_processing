6ea28cad0743733cb1c971f8f872fc6fe27c5b89
Jack Gisby <hhz205@frontend11.apocrita>
2023-10-20 11:54:01 +0100

    Perform PCA on samples and remove labels from plot

--- bin/qc.R
+++ bin/qc.R
@@ -13,11 +13,12 @@ theme_update(text = element_text(size = 8))
 
 filtered_logcpm <- read.csv("filtered_logcpm.csv", row.names = 1)
 
-res.pca <- prcomp(filtered_logcpm, center = TRUE, scale. = TRUE)
+res.pca <- prcomp(t(filtered_logcpm), center = TRUE, scale. = TRUE)
 
 png("qc_pca.png")
 factoextra::fviz_pca_ind(
     res.pca, 
-    addEllipses=FALSE
+    addEllipses=FALSE, 
+    geom="point"
 )
 dev.off()
